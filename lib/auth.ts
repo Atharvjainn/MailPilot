46ee3adf45dce6daeef1836712ab28eeb0c50317
Atharvjainn <jainatharv20@gmail.com>
2026-02-19 20:16:08 +0530

    configured better-auth and postgres

---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import {betterAuth} from 'better-auth';
+import { nextCookies } from 'better-auth/next-js';
+import { prisma } from './prisma'
+import  { prismaAdapter } from 'better-auth/adapters/prisma';
+
+export const auth = betterAuth({
+    database : prismaAdapter(prisma,{provider : 'postgresql'}),
+    socialProviders : {
+        google : {
+            clientId : process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
+        }
+    },
+    plugins : [nextCookies()]
+})
+
